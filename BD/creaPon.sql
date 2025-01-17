28bbea63396fc0db658a392a381207ce3934b7da
KillianCARRAT <143808966+KillianCARRAT@users.noreply.github.com>
2025-01-17 18:17:48 +0100

    Update creaPon.sql

--- BD/creaPon.sql
+++ BD/creaPon.sql
@@ -1,78 +1,56 @@
--- Table ROLE
 CREATE TABLE ROLE (
-    id_role INT PRIMARY KEY,
+    id_role INTEGER PRIMARY KEY,
     name TEXT
 );
 
--- Table UTILISATEUR
 CREATE TABLE UTILISATEUR (
-    id_utilisateur INT PRIMARY KEY,
+    id_utilisateur INTEGER PRIMARY KEY,
     nom_utilisateur TEXT,
     prenom_utilisateur TEXT,
     tel_utilisateur TEXT,
     poids_utilisateur TEXT,
     email_utilisateur TEXT,
     mdp_utilisateur TEXT,
-    role_id INT,
+    role_id INTEGER,
     active BOOLEAN DEFAULT TRUE,
     fs_uniquifier VARCHAR(255) UNIQUE,
-    FOREIGN KEY (role_id) REFERENCES ROLE(id_role)
+    FOREIGN KEY (role_id) REFERENCES ROLE (id_role)
 );
 
--- Table CONTACT
 CREATE TABLE CONTACT (
-    id_contact INT PRIMARY KEY,
+    id_contact INTEGER PRIMARY KEY,
     concerne TEXT,
     sujet TEXT,
     contenu TEXT,
-    id_utilisateur INT,
-    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+    id_utilisateur INTEGER,
+    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id_utilisateur)
 );
 
--- Table PONEY
 CREATE TABLE PONEY (
-    id_poney INT PRIMARY KEY,
+    id_poney INTEGER PRIMARY KEY,
     nom_poney TEXT,
-    capacite_poney INT
+    capacite_poney INTEGER
 );
 
--- Table RESERVATION
 CREATE TABLE RESERVATION (
-    id_reservation INT PRIMARY KEY,
-    id_poney INT,
-    id_utilisateur INT,
-    FOREIGN KEY (id_poney) REFERENCES PONEY(id_poney),
-    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+    id_reservation INTEGER PRIMARY KEY,
+    id_poney INTEGER,
+    id_utilisateur INTEGER,
+    id_seance INTEGER,
+    FOREIGN KEY (id_poney) REFERENCES PONEY (id_poney),
+    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id_utilisateur),
+    FOREIGN KEY (id_seance) REFERENCES SEANCE (id_seance)
 );
 
--- Table RESERVATION_UTILISATEUR
-CREATE TABLE RESERVATION_UTILISATEUR (
-    id_utilisateur INT,
-    id_reservation INT,
-    PRIMARY KEY (id_utilisateur, id_reservation),
-    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur),
-    FOREIGN KEY (id_reservation) REFERENCES RESERVATION(id_reservation)
-);
-
--- Table RESERVATION_SEANCE
-CREATE TABLE RESERVATION_SEANCE (
-    id_seance INT,
-    id_reservation INT,
-    PRIMARY KEY (id_seance, id_reservation),
-    FOREIGN KEY (id_seance) REFERENCES SEANCE(id_seance),
-    FOREIGN KEY (id_reservation) REFERENCES RESERVATION(id_reservation)
-);
-
--- Table SEANCE
 CREATE TABLE SEANCE (
-    id_seance INT PRIMARY KEY,
-    annee_seance INT,
-    semaine_seance INT,
-    jour_seance INT,
+    id_seance INTEGER PRIMARY KEY,
+    annee_seance INTEGER,
+    semaine_seance INTEGER,
+    jour_seance INTEGER,
     heure_debut_seance TIME,
     heure_fin_seance TIME,
-    nb_places_seance INT,
+    nb_places_seance INTEGER,
     active BOOLEAN DEFAULT TRUE,
-    moniteur_id INT,
-    FOREIGN KEY (moniteur_id) REFERENCES UTILISATEUR(id_utilisateur)
+    moniteur_id INTEGER,
+    FOREIGN KEY (moniteur_id) REFERENCES UTILISATEUR (id_utilisateur)
 );
